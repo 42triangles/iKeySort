54536f9121612f80b48a699538447b75927d2909
Nail Sharipov <nailxsharipov@gmail.com>
2025-09-13 09:53:06 +0300

    fix cpu count

--- iKeySort/src/sort/parallel/cpu_count.rs
+++ iKeySort/src/sort/parallel/cpu_count.rs
@@ -9,19 +9,25 @@ impl CPUCount {
     #[cfg(not(debug_assertions))]
     #[inline(always)]
     pub(super) fn should_parallel(len: usize) -> Option<usize> {
-        const MIN_LEN_PER_TASK: usize = 32_000;
-        let cpu = CPUCount::count().max(1);
-        if len >= cpu * MIN_LEN_PER_TASK {
-            Some(cpu)
-        } else {
-            None
+        const MIN_LEN_PER_TASK_POWER: u32 = 15;
+        const MIN_LEN_PER_TASK: usize = 1 << MIN_LEN_PER_TASK_POWER;
+
+        if len < 2 * MIN_LEN_PER_TASK {
+            return None;
         }
+
+        let cpu = CPUCount::count();
+        if cpu <= 1 {
+            return None;
+        }
+
+        let tasks = (len >> MIN_LEN_PER_TASK_POWER).min(cpu);
+        Some(tasks)
     }
 
     #[cfg(debug_assertions)]
     #[inline(always)]
     pub(super) fn should_parallel(_: usize) -> Option<usize> {
         Some(CPUCount::count().max(1))
-        // Some(1)
     }
 }
